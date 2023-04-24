7aaac00c916805325ecebd4c4ea8090eeca1f729
Charlotte.Hong <hunandy14@gmail.com>
2023-04-24 14:32:45 +0900

    修正錯誤與小功能 1. 因為mssql上會完全照著data的樣子上傳，不須把雙引號變成兩個 2. 修正編程風格，改回自己習慣的 3. 專案已經選定是CSV了，就不公開 Terminator 與 RowTerminator 了，沒必要直接訂死就好 4. 公開 Encoding 給使用者設定

--- Import-MssqlCsv.ps1
+++ Import-MssqlCsv.ps1
@@ -1,17 +1,19 @@
 # 移除 CSV 數據中的雙引號
 function Remove-CsvQuotes {
     param (
-        [Parameter(ValueFromPipeline = $true)]
-        [psobject[]]$InputObject,
-        [string]$InputPath,
-        [Parameter(Mandatory = $true)]
-        [string]$OutputPath,
-        [string]$Delimiter = ',',
-        [System.Text.Encoding]$Encoding = (New-Object System.Text.UTF8Encoding $False),
-        [switch]$RemoveHeader
+        [Parameter(ParameterSetName = "InputObject", Mandatory, ValueFromPipeline)]
+        [psobject[]] $InputObject,
+        [Parameter(ParameterSetName = "InputPath", Mandatory, ValueFromPipeline)]
+        [string] $InputPath,
+        [Parameter(ParameterSetName = "", Mandatory)]
+        [string] $OutputPath,
+        [Parameter(ParameterSetName = "")]
+        [text.encoding] $Encoding = (New-Object System.Text.UTF8Encoding $False),
+        [switch] $RemoveHeader
     )
 
     begin {
+        [string] $Delimiter = ','
         $directory = Split-Path -Path $OutputPath -Parent
         if (-not (Test-Path $directory)) { New-Item -ItemType Directory -Path $directory | Out-Null }
         $writer = New-Object System.IO.StreamWriter -ArgumentList $OutputPath, $false, $Encoding
@@ -25,7 +27,7 @@ function Remove-CsvQuotes {
                 $properties = $obj | Get-Member -MemberType Properties
                 foreach ($prop in $properties) {
                     if (-not [string]::IsNullOrEmpty($line)) { $line += $Delimiter }
-                    $value = $obj.$($prop.Name) -replace '"', '""'
+                    $value = $obj.$($prop.Name) # -replace('"', '""')
                     $line += $value
                 }; $writer.WriteLine($line)
             }
@@ -40,7 +42,7 @@ function Remove-CsvQuotes {
                 $fields = $line.Split($Delimiter)
                 $newLine = ""
                 for ($i = 0; $i -lt $fields.Length; $i++) {
-                    $cleanField = $fields[$i].Trim('"')
+                    $cleanField = $fields[$i] -replace('^"|"$') # -replace('"', '""')
                     if ($i -gt 0) { $newLine += $Delimiter }
                     $newLine += $cleanField
                 }; $writer.WriteLine($newLine)
@@ -52,32 +54,35 @@ function Remove-CsvQuotes {
         $writer.Close()
     }
 } # Remove-CsvQuotes -InputPath 'csv\Data.csv' -OutputPath 'data\Data.data' -RemoveHeader
-
+# Import-Csv 'csv\Data.csv'|Remove-CsvQuotes -OutputPath 'data\Data.data' -RemoveHeader
 
 
 # 上傳 CSV 到 MSSQL資料庫
 function Import-MssqlCsv {
     [CmdletBinding()]
     param(
-        [Parameter(Mandatory)]
+        [Parameter(ParameterSetName = "", Mandatory)]
         [string] $ServerName,
-        [Parameter(Mandatory)]
+        [Parameter(ParameterSetName = "", Mandatory)]
         [string] $UserName,
-        [Parameter(Mandatory)]
+        [Parameter(ParameterSetName = "", Mandatory)]
         [string] $Passwd,
-        [Parameter(Mandatory)]
+        [Parameter(ParameterSetName = "", Mandatory)]
         [string] $Table,
-        [Parameter(Mandatory)]
+        [Parameter(ParameterSetName = "", Mandatory)]
         [string] $CsvPath,
-        [string] $Terminator = ',',
-        [string] $RowTerminator = "`n",
+        [Parameter(ParameterSetName = "")]
+        [text.encoding] $Encoding = (New-Object System.Text.UTF8Encoding $False),
+        [switch] $NonHeaderFile,
         [switch] $OutNull
     )
     
     begin {
         $dataPath = "data\Data.data"
-        Remove-CsvQuotes -InputPath $CsvPath -OutputPath $dataPath -RemoveHeader
+        Remove-CsvQuotes -InputPath $CsvPath -OutputPath $dataPath -RemoveHeader:(!$NonHeaderFile) -Encoding:$Encoding
         $CsvPath = $dataPath
+        [string] $Terminator = ','
+        [string] $RowTerminator = "`n"
     }
     
     process {
@@ -107,3 +112,4 @@ function Import-MssqlCsv {
         }
     }
 } # Import-MssqlCsv -ServerName "192.168.3.123,1433" -UserName "kaede" -Passwd "1230" -Table "[CHG].[CHG].[TEST]" -CsvPath "csv\Data.csv" | Out-Null
+# Import-MssqlCsv -ServerName "192.168.3.123,1433" -UserName "kaede" -Passwd "1230" -Table "[CHG].[CHG].[TEST]" -CsvPath "csv\Data.data" -NonHeaderFile | Out-Null
