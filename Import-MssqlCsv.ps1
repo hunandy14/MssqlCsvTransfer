11af95c4e91a037e9491ca946a15b3dcb969dd59
Charlotte.Hong <hunandy14@gmail.com>
2023-05-08 00:16:52 +0900

    Remove-CsvQuotes 移除多餘的代碼，僅保留該專案所需的部分

--- Import-MssqlCsv.ps1
+++ Import-MssqlCsv.ps1
@@ -1,60 +1,52 @@
 # 移除 CSV 數據中的雙引號
 function Remove-CsvQuotes {
     param (
-        [Parameter(ParameterSetName = "InputObject", Mandatory, ValueFromPipeline)]
-        [psobject[]] $InputObject,
-        [Parameter(ParameterSetName = "InputPath", Mandatory, ValueFromPipeline)]
+        [Parameter(Position = 0, ParameterSetName = "", Mandatory)]
         [string] $InputPath,
-        [Parameter(ParameterSetName = "", Mandatory)]
+        [Parameter(ParameterSetName = "")]
         [string] $OutputPath,
         [Parameter(ParameterSetName = "")]
         [text.encoding] $Encoding = (New-Object System.Text.UTF8Encoding $False),
         [switch] $RemoveHeader
     )
+    [IO.Directory]::SetCurrentDirectory(((Get-Location -PSProvider FileSystem).ProviderPath))
+    
+    # 設定分隔符號
+    [string] $Delimiter = ','
 
-    begin {
-        [string] $Delimiter = ','
-        $directory = Split-Path -Path $OutputPath -Parent
-        if (-not (Test-Path $directory)) { New-Item -ItemType Directory -Path $directory | Out-Null }
-        $writer = New-Object System.IO.StreamWriter -ArgumentList $OutputPath, $false, $Encoding
-        $headerProcessed = $false
-    }
-
-    process {
-        if ($InputObject) {
-            foreach ($obj in $InputObject) {
-                $line = ""
-                $properties = $obj | Get-Member -MemberType Properties
-                foreach ($prop in $properties) {
-                    if (-not [string]::IsNullOrEmpty($line)) { $line += $Delimiter }
-                    $value = $obj.$($prop.Name) # -replace('"', '""')
-                    $line += $value
-                }; $writer.WriteLine($line)
+    # 確認輸入檔案存在
+    if (!(Test-Path -PathType:Leaf $InputPath)) { Write-Error "The [`$InputPath:: `"$InputPath`"] does not exist."; return }
+    $InputPath = [IO.Path]::GetFullPath($InputPath)
+    # 創建輸出檔案
+    if ($OutputPath) {
+        $OutputPath = [IO.Path]::GetFullPath($OutputPath)
+        if (!(Test-Path $OutputPath)) { New-Item $OutputPath -Force -EA:Stop | Out-Null }
+    } else { $OutputPath = New-TemporaryFile }
+    
+    # 流處理檔案
+    $headerProcessed = $false
+    $writer = New-Object System.IO.StreamWriter -ArgumentList $OutputPath, $false, $Encoding
+    $reader = New-Object System.IO.StreamReader -ArgumentList $InputPath, $Encoding
+    if ($reader) {
+        while (!$reader.EndOfStream) {
+            $line = $reader.ReadLine()
+            if ($RemoveHeader -and !$headerProcessed) {
+                $headerProcessed = $true
+                continue
             }
-        } elseif ($InputPath) {
-            $reader = New-Object System.IO.StreamReader -ArgumentList $InputPath, $Encoding
-            while (-not $reader.EndOfStream) {
-                $line = $reader.ReadLine()
-                if ($RemoveHeader -and -not $headerProcessed) {
-                    $headerProcessed = $true
-                    continue
-                }
-                $fields = $line.Split($Delimiter)
-                $newLine = ""
-                for ($i = 0; $i -lt $fields.Length; $i++) {
-                    $cleanField = $fields[$i] -replace('^"|"$') # -replace('"', '""')
-                    if ($i -gt 0) { $newLine += $Delimiter }
-                    $newLine += $cleanField
-                }; $writer.WriteLine($newLine)
-            }; $reader.Close()
-        }
-    }
-
-    end {
-        $writer.Close()
-    }
+            $fields = $line.Split($Delimiter)
+            $newLine = ""
+            for ($i = 0; $i -lt $fields.Length; $i++) {
+                $cleanField = $fields[$i] -replace('^"|"$') # -replace('"', '""')
+                if ($i -gt 0) { $newLine += $Delimiter }
+                $newLine += $cleanField
+            }; $writer.WriteLine($newLine)
+        }; $reader.Close()
+    }; $writer.Close()
+    
+    return $OutputPath
 } # Remove-CsvQuotes -InputPath 'csv\Data.csv' -OutputPath 'data\Data.data' -RemoveHeader
-# Import-Csv 'csv\Data.csv'|Remove-CsvQuotes -OutputPath 'data\Data.data' -RemoveHeader
+# $tmp = Remove-CsvQuotes -InputPath 'csv\Data.csv' -RemoveHeader; if ($tmp) { $tmp; Remove-Item "$($tmp -replace '.tmp$').tmp" }
 
 
 
@@ -98,7 +90,7 @@ function Import-MssqlCsv {
             if ($RowsCopied -eq 0) { $HasError = $true }
         } else { $HasError = $true }
         
-        if (-not $OutNull) {
+        if (!$OutNull) {
             if ($HasError) {
                 $ErrMsg = $Output -join ', '
                 Write-Error "Error:: BCP command execution failed. Details: $ErrMsg"
@@ -121,5 +113,5 @@ function Import-MssqlCsv {
             Message = $Output -match ".+" -notmatch "Starting copy..."
         }
     }
-} # Import-MssqlCsv -ServerName "192.168.3.123,1433" -UserName "kaede" -Passwd "1230" -Table "[CHG].[CHG].[TEST]" -CsvPath "csv\Data.csv" | Out-Null
-# Import-MssqlCsv -ServerName "192.168.3.123,1433" -UserName "kaede" -Passwd "1230" -Table "[CHG].[CHG].[TEST]" -CsvPath "csv\Data.data" -NonHeaderFile | Out-Null
+} # Import-MssqlCsv -ServerName "192.168.3.123,1433" -UserName "kaede" -Passwd "1230" -Table "[CHG].[CHG].[TEST]" -CsvPath "csv\Data.csv"
+# Import-MssqlCsv -ServerName "192.168.3.123,1433" -UserName "kaede" -Passwd "1230" -Table "[CHG].[CHG].[TEST]" -CsvPath "data\Data.data" -NonHeaderFile | Out-Null
